223981607fc54993d6527bd7fd0e74d5acd8ccf8
Mugi <54444757+xMugi@users.noreply.github.com>
2024-06-09 13:10:14 +0200

    added ports and background run

--- startqemu.sh
+++ startqemu.sh
@@ -1,6 +1,6 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
-qemu-system-x86_64 -machine q35 -m 1024 -smp cpus=2 -cpu qemu64 \
+nohup qemu-system-x86_64 -machine q35 -m 4024 -smp cpus=6 -cpu qemu64 \
   -drive if=pflash,format=raw,read-only=on,file=$PREFIX/share/qemu/edk2-x86_64-code.fd \
-  -netdev user,id=n1,hostfwd=tcp::2222-:22 -device virtio-net,netdev=n1 \
-  -nographic alpine.img
+  -netdev user,id=n1,hostfwd=tcp::2222-:22,hostfwd=tcp::5900-:5900,hostfwd=tcp::5901-:5901,hostfwd=tcp::8080-:8080,hostfwd=tcp::9000-:9000,hostfwd=tcp::8000-:8000,hostfwd=tcp::9090-:9090,hostfwd=tcp::4000-:4000,hostfwd=tcp::8001-:8001,hostfwd=tcp::19999-:19999,hostfwd=tcp::6000-:6000,hostfwd=tcp::7000-:7000,hostfwd=tcp::8081-:8081,hostfwd=tcp::1443-:1443,hostfwd=tcp::1080-:1080,hostfwd=tcp::9443-:9443 -device virtio-net,netdev=n1 \
+  -nographic alpine.img > qemu.log 2>&1 &
